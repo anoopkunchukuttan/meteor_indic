8fcf2b638b9b382183355719c4fcb40ce78e156e
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2013-09-16 14:07:17 +0530

    script for generating synonym resources from IndoWordnet

---
+++ scripts/generate_syn_tables_from_iwn.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#parameters
+# the directory containing the IndoWordnet database dump
+IWN_DIR=$1
+#Output directory where the data is generated in a format as required by METEOR
+#$OUT_DIR/meteor_data will contain the synonym resources to be used in METEOR
+OUT_DIR=$2
+
+mkdir -p $OUT_DIR/iwn_data $OUT_DIR/meteor_data
+cd $OUT_DIR/iwn_data
+
+for f in `find $IWN_DIR -name '*zip'`
+do
+ #echo $f `basename $f`
+ unzip $f
+done
+
+# use 2-letter ISO 639 codes with invented codes for languages not having a code, indicated by capital letter in the code
+mv ass as
+mv ben bn
+mv bod bD
+mv guj gu
+mv hin hi
+mv kan kn
+mv kas ks
+mv kon kK
+mv mal ml
+mv man mP
+mv mar mr
+mv nep ne
+mv ori or
+mv pan pa
+mv san sa
+mv tam ta 
+mv tel te
+mv urd ur
+
+for f in `ls`
+do
+ cat $f/{adj.idx,noun.idx,adv.idx,verb.idx} > $f/all.idx
+ sed 's, ,\n,' $f/all.idx > ../meteor_data/$f.synsets
+ touch ../meteor_data/$f.exceptions
+ touch ../meteor_data/$f.relations
+done
